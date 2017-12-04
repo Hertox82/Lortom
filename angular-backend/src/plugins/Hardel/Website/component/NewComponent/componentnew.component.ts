39ff5568b04adbc665da175e05f1fdc439f498e5
Hernan Ariel De Luca <hernan@lastrega.com>
2017-12-04 15:28:02 +0100

    Removed all references about Elements

--- angular-backend/src/plugins/Hardel/Website/component/NewComponent/componentnew.component.ts
+++ angular-backend/src/plugins/Hardel/Website/component/NewComponent/componentnew.component.ts
@@ -3,10 +3,7 @@
  */
 
 import {Component, OnInit} from "@angular/core";
-import {
-    LortomComponent,
-    LortomElement, LtElementComp
-} from "@Lortom/plugins/Hardel/Website/Services/website.interfaces";
+import { LortomComponent } from "@Lortom/plugins/Hardel/Website/Services/website.interfaces";
 import {WebsiteService} from "@Lortom/plugins/Hardel/Website/Services/website.service";
 import {Router} from "@angular/router";
 @Component({
@@ -22,14 +19,11 @@ export class NewComponent implements OnInit
     id : number;
     private sub : any;
     isEdit : boolean;
-    listElements = [];
-    filteredList : LortomElement[];
     query : string;
     config : any;
 
     constructor(private ncsService : WebsiteService, private router : Router){
         this.isEdit = false;
-        this.filteredList = [];
         this.query = '';
         this.config = {
             lineNumbers: true,
@@ -44,74 +38,14 @@ export class NewComponent implements OnInit
             name : '',
             check : false,
             appearance : '',
-            elements : []
         }
     }
     ngOnInit(){
         this.retriveElement();
     }
 
-    private retriveElement()
-    {
-        this.ncsService.getElementsFrom().subscribe(
-            (elements : LortomElement []) => {
-                this.listElements = elements;
-            }
-        );
+    private retriveElement() {
         this.cloneComponent();
-    }
-
-    /**
-     * This function filter permission for research
-     */
-    filter(){
-        if (this.query !== "") {
-            this.filteredList = this.listElements.filter(function(el){
-                return el.name.toLowerCase().indexOf(this.query.toLowerCase()) > -1;
-            }.bind(this));
-        }
-    }
-
-    /**
-     * This function delete Permission from role.permissions
-     * @param item
-     */
-    eraseElement(item) {
-        // cancella il permesso
-        let index = this.component.elements.indexOf(item);
-
-        if(index > -1)
-        {
-            this.component.elements.splice(index,1);
-        }
-
-    }
-
-    /**
-     * This Function add Permission at the moment to role.permissions
-     * @param item
-     */
-    addElement(item : LortomElement) {
-        //aggiunge un permesso
-        this.filteredList = [];
-        this.query = item.name;
-
-        this.component.elements.push(this.toElementComponent(item));
-    }
-
-    toElementComponent(item : LortomElement) : LtElementComp
-    {
-        let it : LtElementComp = {
-            id : -1,
-            idElement : item.id,
-            name : item.name,
-            Object : item.Object,
-            functions : item.functions,
-            appearance : item.appearance,
-            check : false
-        };
-
-        return it;
     }
 
     /**
@@ -148,22 +82,14 @@ export class NewComponent implements OnInit
 
     isEqual(v,v2) : boolean
     {
-        return (v.name == v2.name) && (v.elements.length == v2.elements.length)
+        return (v.name == v2.name) && (v.appearance == v2.appearance)
     }
 
     /**
      * This function clone the Role
      */
     cloneComponent(){
-        let elements: LtElementComp[] = [];
-
-        for(let perm of this.component.elements)
-        {
-            elements.push(perm);
-        }
-
         this.copyComponent = Object.assign({},this.component);
-        this.copyComponent.elements = elements;
     }
 
     /**
@@ -171,14 +97,6 @@ export class NewComponent implements OnInit
      */
     cloneCopyComponent()
     {
-        let elements: LtElementComp[] = [];
-
-        for(let perm of this.copyComponent.elements)
-        {
-            elements.push(perm);
-        }
-
         this.component = Object.assign({},this.copyComponent);
-        this.component.elements = elements;
     }
 }
